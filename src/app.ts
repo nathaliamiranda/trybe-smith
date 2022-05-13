08f44966e04c1c2377a1971becc13bac26cb62db
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-13 15:33:12 -0300

    feat: import dotenv and product router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import ProductRouter from './routes/product.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(ProductRouter);
 
 export default app;
