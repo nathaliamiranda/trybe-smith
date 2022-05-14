765cdc5bec33127e7524367f09f3c4045fde0df1
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-14 19:21:54 -0300

    feat: create index routes

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import productRouter from './product.routes';
+import userRouter from './user.routes';
+
+const router = Router();
+
+router.use('/products', productRouter);
+router.use('/users', userRouter);
+
+export default router;
