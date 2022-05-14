1e3e9efd0018b8f8ee538cf16733c42630df4818
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-14 17:30:42 -0300

    feat: create router post product

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import ProductRouter from './routes/product.routes';
+import error from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(ProductRouter);
+
+app.use(error);
 
 export default app;
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product/getall.product.controler';
+import productValidate from '../middlewares/product';
 
 const router = Router();
 
 const productController = new ProductController();
 
 router.get('/products', productController.getAll);
+router.post('/products', productValidate, productController.create);
 
 export default router;
