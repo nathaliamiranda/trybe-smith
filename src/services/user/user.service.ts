12721b5a5cdd668576fbc57c60af31dccf540ca0
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-14 19:22:36 -0300

    feat: create user service

---
+++ src/services/user/user.service.ts
@@ -0,0 +1,17 @@
+import connection from '../../models/connection';
+import UserModel from '../../models/user.model';
+import IUser from '../../interfaces/user.interface';
+
+class UserService {
+  public model: UserModel;
+
+  constructor() {
+    this.model = new UserModel(connection);
+  }
+
+  public create(user: IUser): Promise<IUser> {
+    return this.model.create(user);
+  }
+}
+
+export default UserService;
