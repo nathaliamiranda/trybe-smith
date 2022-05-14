8d599babd804eed72bebd906bd9a0a46fb589e10
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-14 19:22:15 -0300

    feat: create user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,22 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import IUser from '../interfaces/user.interface';
+
+export default class ProductModel {
+  public connection: Pool;
+  
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+  
+  public async create(user: IUser): Promise<IUser> {
+    const { username, classe, password, level } = user;
+
+    const [result] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (username, classe, password, level) VALUES (?, ?, ?, ?)',
+      [username, classe, password, level],
+    );
+    const { insertId: id } = result;
+    const newUser: IUser = { id, username, classe, password, level };
+    return newUser;
+  }
+}
