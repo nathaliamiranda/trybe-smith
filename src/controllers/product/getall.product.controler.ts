6465983f021a725a716ee853c0e3adf9fde57729
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-14 17:30:09 -0300

    feat: create funtion create product

--- src/controllers/product/getall.product.controler.ts
+++ src/controllers/product/getall.product.controler.ts
@@ -8,6 +8,13 @@ class ProductController {
     const products = await this.productService.getAll();
     res.status(200).json(products);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    res.status(201).json(productCreated);
+  };
 }
 
 export default ProductController;
