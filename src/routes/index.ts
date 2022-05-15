db8ee3481e7da516abeec2d24260b8098b1f11de
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-15 14:24:24 -0300

    feat: create order route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productRouter from './product.routes';
 import userRouter from './user.routes';
+import ordersRouter from './orders.routes';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/users', userRouter);
+router.use('/orders', ordersRouter);
 
 export default router;
