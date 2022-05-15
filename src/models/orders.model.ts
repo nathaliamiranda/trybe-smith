57dfc301fbbcc39342174444c9b8639d7503b317
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-15 14:22:57 -0300

    feat: create model order

---
+++ src/models/orders.model.ts
@@ -0,0 +1,26 @@
+import { Pool, RowDataPacket } from 'mysql2/promise';
+import IOrder from '../interfaces/orders.interface';
+
+class OrderModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IOrder[]> {
+    const [orders] = await this.connection
+      .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Orders');
+
+    const [products] = await this.connection
+      .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products');
+
+    const result = orders.map((ord) => ({
+      ...ord, 
+      productsIds: products
+        .filter((prod) => prod.orderId === ord.id)
+        .map((e) => e.id) }));
+    return result as IOrder[];
+  }
+}
+export default OrderModel;
