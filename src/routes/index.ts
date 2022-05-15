479a538fba016529d64eb0bec9c22054a97452a8
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-15 16:29:05 -0300

    feat: import loginRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productRouter from './product.routes';
 import userRouter from './user.routes';
 import ordersRouter from './orders.routes';
+import loginRouter from './login.routes';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/users', userRouter);
 router.use('/orders', ordersRouter);
+router.use('/login', loginRouter);
 
 export default router;
