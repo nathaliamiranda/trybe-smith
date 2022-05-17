f4ed1341d48509f7c3126f8d10d586346b860b74
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-17 09:43:21 -0300

    refactor: token generate

--- src/helpers/generate.token.ts
+++ src/helpers/generate.token.ts
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
-import IUsername from '../interfaces/username.interface';
+import IPayloadToken from '../interfaces/verify.token';
 
 const jwtConfig: jwt.SignOptions = {
   expiresIn: '2d',
   algorithm: 'HS256',
 };
 
-const SECRET = 'Jesus';
+const SECRET = '1D24JGU4598FEH402JLB';
 
-export const signJwt = (payload: IUsername) => jwt.sign({ data: payload }, SECRET, jwtConfig);
+export const signJwt = (payload: IPayloadToken) => jwt.sign({ data: payload }, SECRET, jwtConfig);
 export const verifyJwt = (payload: string) => jwt.verify(payload, SECRET);
