4047f818565d2ec6e4592579ed5a28558ab47dff
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-17 09:50:33 -0300

    feat: create route post

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order/order.controller';
+import isValidOrder from '../middlewares/order';
+import verify from '../middlewares/auth';
 
 const router = Router();
 
 const orderController = new OrderController();
 
 router.get('/', orderController.getAll);
+router.post('/', verify, isValidOrder, orderController.create);
 
 export default router;
